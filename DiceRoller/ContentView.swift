ab54101f50b838e7a10d1818479b50f516eab53b
Jessica Soares <jess.soares@hotmail.com>
2023-11-17 16:26:04 +0000

    up

--- DiceRoller/ContentView.swift
+++ DiceRoller/ContentView.swift
@@ -14,25 +14,35 @@ struct ContentView: View {
         VStack {
             Text("Dice Roller")
                 .font(.largeTitle.lowercaseSmallCaps())
+                .foregroundStyle(.white)
             
             HStack {
                 ForEach(1...numberOfDice, id: \.self) { _ in
                     DiceView()
+                        .foregroundStyle(.white)
                 }
             }
             
             HStack {
-                Button("Remove Dice") {
+                Button("Remove Dice", systemImage: "minus.circle.fill") {
                     numberOfDice -= 1
                 }
                 
-                Button("Add Dice") {
+                .disabled(numberOfDice == 1)
+                
+                Button("Add Dice", systemImage: "plus.circle.fill") {
                     numberOfDice += 1
                 }
+                .disabled(numberOfDice == 5)
             }
             .padding()
+            .labelStyle(.iconOnly)
+            .font(.title)
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.appBackground)
+        .tint(.white)
     }
 }
 
